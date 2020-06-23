a4d489e08bb6d76e702ee0d3f1c7531b6d0c418f
Ian Lucas <ianlucas_ferreira@hotmail.com>
2020-06-22 21:25:10 -0300

    Add Label component

---
+++ src/components/GuiLabel.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="gui-label">
+    <slot />
+  </span>
+</template>
+
+<style>
+.gui-label {
+  display: inline-block;
+  padding: 3px 0;
+}
+</style>
--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,6 @@
 import GuiButton from './components/GuiButton.vue'
 import GuiFullscreen from './components/GuiFullscreen.vue'
+import GuiLabel from './components/GuiLabel.vue'
 import GuiPanel from './components/GuiPanel.vue'
 import GuiSelect from './components/GuiSelect.vue'
 import GuiSplit from './components/GuiSplit.vue'
@@ -9,6 +10,7 @@ import GuiToolbar from './components/GuiToolbar.vue'
 export {
   GuiButton,
   GuiFullscreen,
+  GuiLabel,
   GuiPanel,
   GuiSelect,
   GuiSplit,
