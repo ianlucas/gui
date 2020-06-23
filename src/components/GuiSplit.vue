b1836fdf556dc9b93cfe6e33e1be8a61101b0db5
Ian Lucas <ianlucas_ferreira@hotmail.com>
2020-06-22 21:04:40 -0300

    Add drag event to Split component

--- src/components/GuiSplit.vue
+++ src/components/GuiSplit.vue
@@ -58,7 +58,10 @@ export default {
       sizes,
       minSizes,
       direction: 'vertical',
-      gutterSize: this.gutterSize
+      gutterSize: this.gutterSize,
+      onDrag: () => {
+        this.$emit('drag')
+      }
     })
   }
 }
